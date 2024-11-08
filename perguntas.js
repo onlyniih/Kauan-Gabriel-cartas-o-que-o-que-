0fd92744f68d9245c7b833e2156b4c4a0b974c41
onlyniih <ncristine002@gmail.com>
2024-11-08 14:24:05 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'O que é o que é',
+    'Anda com os pés na cabeça?',
+    'O piolho'
+)
+
+criaCartao(
+    'O que é o que é',
+    'Quanto mais se tira mais se aumenta?',
+    'O buraco'
+)
+
+criaCartao(
+    'O que é o que é',
+    'Mesmo atravessando o rio não se molha?',
+    'A ponte'
+)
+
+criaCartao(
+    'O que é o que é',
+    'Tem no meio do ovo?',
+    'o "V" '
+)
